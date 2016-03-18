22e6856f33eb651354fcb787957a197975bd8c41
Neil Francis Ramirez Hipona <nferocious76@gmail.com>
2016-03-19 02:34:05 +0800

    Project workspace

---
+++ Example/Pods/Target Support Files/AutoCompleteTextField/AutoCompleteTextField-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double AutoCompleteTextFieldVersionNumber;
+FOUNDATION_EXPORT const unsigned char AutoCompleteTextFieldVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-AutoCompleteTextField_Tests/Pods-AutoCompleteTextField_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_AutoCompleteTextField_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_AutoCompleteTextField_TestsVersionString[];
+
